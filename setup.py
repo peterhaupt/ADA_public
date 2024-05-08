1652fd5b655c6a2e7c30bde265d8ac8de268dd64
David Pennekamp <40147198+DavidPennekamp@users.noreply.github.com>
2024-05-08 11:22:07 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ADA_public',
+    version='1.0.0',
+    url='https://github.com/peterhaupt/ADA_public.git',
+    author='Google',
+    author_email='google@gmail.com',
+    description='Pubsub',
+    packages=find_packages(),    
+    install_requires=["google-cloud-pubsub"],
+)
