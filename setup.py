9116902aa2e3e99f7c472c6a056ed85288e10e57
David Pennekamp <40147198+DavidPennekamp@users.noreply.github.com>
2024-05-08 11:25:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ADA_public',
+    name='ADApublic',
     version='1.0.0',
     url='https://github.com/peterhaupt/ADA_public.git',
     author='Google',
