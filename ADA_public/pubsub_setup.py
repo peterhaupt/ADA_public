cec6345cbd5a3b363c12c6fe065e333a2dcb7848
David Pennekamp <40147198+DavidPennekamp@users.noreply.github.com>
2024-05-08 18:28:49 +0200

    wrongly encoded

--- ADA_public/pubsub_setup.py
+++ ADA_public/pubsub_setup.py
@@ -43,9 +43,8 @@ def publish_message(project_id, topic_id, message):
     # in the form `projects/{project_id}/topics/{topic_id}`
     topic_path = publisher.topic_path(project_id, topic_id)
 
-    data = message.encode("utf-8")
     # When you publish a message, the client returns a future.
-    future = publisher.publish(topic_path, data)
+    future = publisher.publish(topic_path, message)
     print("Future returned:", future.result())
 
 
